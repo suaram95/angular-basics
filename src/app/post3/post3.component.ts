32bdd3fff4467d270a5c40db6577331af971001b
suaram95 <aram/*java2020>
2021-02-23 14:01:39 +0400

    creating components with command ng g c post2 and post3 --scipTests

---
+++ src/app/post3/post3.component.ts
@@ -0,0 +1,9 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-post3',
+  templateUrl: './post3.component.html',
+  styleUrls: ['./post3.component.css']
+})
+export class Post3Component {
+}
